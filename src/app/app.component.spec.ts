4a80c6394ae838ef96a0186483a42c236b804db4
Jai Chaturvedi <jaichaturvedi@fico.com>
2019-09-04 09:38:33 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ang-uni'`, () => {
+  it(`should have as title 'ivy-firebase'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ang-uni');
+    expect(app.title).toEqual('ivy-firebase');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ang-uni!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ivy-firebase!');
   });
 });
